1432942b3ec66b66cf566c4aa58667569e27f3c3
Dang Huynh <hhdang6637@gmail.com>
2019-07-04 09:38:36 +0000

    duplicate exception

--- exception/exception_in_destructor/exception_in_destructor.cpp
+++ exception/exception_in_destructor/exception_in_destructor.cpp
@@ -7,15 +7,26 @@
 
 class A1
 {
-    A a;
 public:
     A1(){
         FUNC_TRACE()
     };
     virtual ~A1() noexcept(false) {
         FUNC_TRACE()
-        A a;
         throw std::runtime_error("exception of A1 detructor");
+    };
+};
+
+class A2
+{
+public:
+    A2(){
+        FUNC_TRACE()
+    };
+    virtual ~A2() noexcept(false) {
+        FUNC_TRACE()
+        A1 a1;
+        throw std::runtime_error("exception of A2 detructor");
     };
 };
 
@@ -23,7 +34,7 @@ int main(int argc __attribute__((unused)), char const *argv[] __attribute__((unu
 {
     try
     {
-        A1 a1;
+        A2 a2;
     }
     catch (std::exception &e)
     {
